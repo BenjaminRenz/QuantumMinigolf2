8882e7dac30c3b5bd8149a375bce5f8fa5b94301
Benjamin Renz <appswert@gmail.com>
2017-12-23 00:29:36 +0100

    Fix shader compilation

--- QMG2/main.c
+++ QMG2/main.c
@@ -25,8 +25,8 @@ int main(int argc, char* argv[]){
     if(GLEW_OK != err){
         printf("Error: glewInit() failed.");
     }
-
-    printf("OpenGl Version: %s\n",glGetString(GL_VERSION));
+    printf("QuantumMinigolf v2:\n");
+    printf("using OpenGl Version: %s\n",glGetString(GL_VERSION));
     //Register Callbacks for user input
     glfwSetKeyCallback(MainWindow,key_callback);
     glfwSetMouseButtonCallback(MainWindow, mouse_button_callback);
@@ -111,6 +111,7 @@ void mouse_button_callback(GLFWwindow* window, int button,int action, int mods){
  *
  */
 void CompileShaderFromFile(char FilePath[] ,GLuint shaderType){
+    //read from file into heap memory
     FILE* filepointer=fopen(FilePath,"rb");                  //open specified file in read only mode
     if(filepointer==NULL){
         printf("Error: Filepointer to shaderfile at %s could not be loaded.",FilePath);
@@ -120,24 +121,29 @@ void CompileShaderFromFile(char FilePath[] ,GLuint shaderType){
     long filelength = ftell(filepointer);               //get filePointer position
     fseek(filepointer,0,SEEK_SET);                      //move file Pointer back to first line of file
     char* filestring = (char*)malloc(filelength+1);     //
-    if(fread(filestring,sizeof(char),filelength,filepointer != filelength)){
+    if(fread(filestring,sizeof(char),filelength,filepointer) != filelength){
         printf("Error: Missing characters in input string");
         return;
     }
-    if(filestring[0]=0xEF&filestring[0]=0xBB&filestring[0]=0xBF){   //Detect if file is utf8 with bom
-
+    if(filestring[0]==0xEF&&filestring[0]==0xBB&&filestring[0]==0xBF){   //Detect if file is utf8 with bom
+        printf("Error: Remove the bom from your utf8 shader file");
     }
     filestring[filelength]=0;                             //Set end of string
     fclose(filepointer);                                  //Close File
-    //GLuint ShaderId= glCreateShader(shaderType);
-    //glShaderSource(ShaderId,1,&filestring,);
-    //glCompileShader(ShaderId);
-    //GLuint compStatus = 0;
-    //if(glGetShaderiv(ShaderId,GL_COMPILE_STATUS,&compStatus)!=GL_TRUE){
-    //    printf("Error: Compilation of shader %d failed\n",ShaderId);
+    const char* ConstFilePointer = filestring;            //opengl wants const pointer
+    //compile shader with opengl
+    GLuint ShaderId= glCreateShader(shaderType);
+    glShaderSource(ShaderId,1,&ConstFilePointer,NULL);
+    glCompileShader(ShaderId);
+    GLint compStatus = 0;
+    GLint logLength = 0;
+    glGetShaderiv(ShaderId,GL_COMPILE_STATUS,&compStatus);
+    glGetShaderiv(ShaderId,GL_INFO_LOG_LENGTH,&logLength);
+    if(compStatus!=GL_TRUE){
+        printf("Error: Compilation of shader %d failed\n",ShaderId);
         //TODO free resources
-    //    return;
-    //}
+        return;
+    }
     free(filestring);                                   //Delete Shader string from heap
     fclose(filepointer);
 }
