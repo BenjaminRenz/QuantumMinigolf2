db9866e8f1428a69bb62b6430973de7620d6bdf5
hiwi <sdkhiwi@SDKPC1.PI5>
2017-12-22 10:12:03 +0100

    Add Key callback test

--- QMG2/main.c
+++ QMG2/main.c
@@ -3,12 +3,13 @@
 #include "libraries/GLEW_2.1.0/include/glew.h"
 #include "libraries/GLFW_3.2.1/include/glfw3.h"
 #include <stdio.h>
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main(int argc, char* argv[]){
     if (!glfwInit()){
         return -1;
     }
-    GLFWwindow* MainWindow = glfwCreateWindow(600, 400, "Quantum Minigolf 2.0", NULL, NULL);
+    GLFWwindow* MainWindow = glfwCreateWindow(1920, 1080, "Quantum Minigolf 2.0", glfwGetPrimaryMonitor(), NULL);
     //GLFWwindow* MainWindow = glfwCreateWindow(1920, 1080, "Quantum Minigolf 2.0", glfwGetPrimaryMonitor(), NULL);
     if (!MainWindow){
         glfwTerminate();
@@ -16,6 +17,7 @@ int main(int argc, char* argv[]){
     }
     glfwMakeContextCurrent(MainWindow);
     GLenum err = glewInit();
+    glfwSetKeyCallback(MainWindow,key_callback);
     if(GLEW_OK != err){
         printf("Error: glewInit() failed.");
     }
@@ -28,4 +30,10 @@ int main(int argc, char* argv[]){
     }
     glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if(key==GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        printf("Escape");
+    }
 }
