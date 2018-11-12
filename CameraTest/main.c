38fd231e27f9fdad678f667f645cc093b8a876e6
Benjamin Renz <appswert@gmail.com>
2018-11-12 01:18:23 +0100

    Camera test

--- CameraTest/main.c
+++ CameraTest/main.c
@@ -1,5 +1,18 @@
 #include <libavcodec/avcodec.h>
+#include <libavdevice/dshow.c>
+#include <dshow.h>
+#include <guiddef.h>
 #include <stdio.h>
+#include <strmif.h>
+int createVideoDevice();
+int createVideoDevice(){
+
+    return 0;
+}
+/*int deleteVideoDevice(){
+    CoUninitialize();
+}*/
+
 int main(void){
-    printf("test");
+    createVideoDevice();
 }
