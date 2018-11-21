1a1fff7ec76924bce2193464d67b723ff2df3b14
Benjamin Renz <appswert@gmail.com>
2018-11-21 06:12:31 +0100

    Add

--- CameraTest/main.c
+++ CameraTest/main.c
@@ -6,6 +6,8 @@
 #include <strmif.h>
 int createVideoDevice();
 int createVideoDevice(){
+    //Used code from https://www.codeproject.com/Articles/12869/Real-time-video-image-processing-frame-grabber-usi
+    //from Ladislav Nevery under "The Code Project Open License"
     if(CoInitializeEx(NULL,COINIT_MULTITHREADED)==S_OK){
         printf("COM library initialized\n");
     }else{
@@ -46,6 +48,35 @@ int createVideoDevice(){
         myPropertyBag->lpVtbl->Read(myPropertyBag,L"DevicePath",&myFieldForDevicePath,0);
         printf("%S\n",myFieldForFriendlyName.bstrVal);
         printf("%S\n",myFieldForDevicePath.bstrVal);
+        myBindContext->lpVtbl->Release(myBindContext);
+
+        //TODO only do this after we selected a camera
+        //Create Graph
+        IGraphBuilder* graph=NULL;
+        CoCreateInstance(&CLSID_FilterGraph,NULL,CLSCTX_INPROC,&IID_IGraphBuilder,(void **)&graph);
+        //Create Filter
+        IBaseFilter* myCameraGraphBaseObj=NULL;
+        CreateBindCtx(0,&myBindContext);
+        myCamera->lpVtbl->BindToObject(myCamera,myBindContext,NULL,&IID_IBaseFilter,(void **)&myCameraGraphBaseObj);
+        graph->lpVtbl->AddFilter(graph,myCameraGraphBaseObj, L"Capture Source");
+        IEnumPins* myOutputpins=0;
+        myCameraGraphBaseObj->lpVtbl->EnumPins(myCameraGraphBaseObj,&myOutputpins);
+        IPin* myOutputpin=0;
+        unsigned long numberOfFetchedOutputPins=0;
+        myOutputpins->lpVtbl->Next(myOutputpins,1,&myOutputpin,&numberOfFetchedOutputPins); //get one output pin
+        graph->lpVtbl->Render(graph,myOutputpin); //Render this output
+
+        IEnumFilters* myFilter=NULL;
+        graph->lpVtbl->EnumFilters(graph,&myFilter);
+        IBaseFilter* rnd=NULL;
+        myFilter->lpVtbl->Next(myFilter,1,&rnd,0);
+        IEnumPins* myRenderPins=0;
+        rnd->lpVtbl->EnumPins(rnd,&myRenderPins);
+        IPin* myRenderPin=0;
+        myRenderPins->lpVtbl->Next(myRenderPins,1,&myRenderPin, 0);
+        IMemInputPin* myMemoryInputPin= 0;
+        myRenderPin->lpVtbl->QueryInterface(myRenderPin,&IID_IMemInputPin,(void**)&myMemoryInputPin);
+
     }
 
 
